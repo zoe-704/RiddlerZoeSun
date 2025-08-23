dd9590ef58d30a868e9fc5c49a465e8c192a4ff0
zoe-704 <sunzoey@gmail.com>
2025-08-22 19:30:27 -0700

    Implement puzzle 1

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,8 @@
-Name: YOUR NAME HERE
-
-| Date   |    Time    |                                                                    Update |
-|:-------|:----------:|--------------------------------------------------------------------------:|
-| Aug 22 |  8-8:30pm  | I started by thinking about my place in the universe.<br/>It was scary... |
-|        |            |                                                             And then I... |
-
+Name: Zoe Sun
+| Time      |  Date  | Update                                                                                                                                                                      |
+|-----------|:------:|:----------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| 11:40-12  | Aug 22 | Saw reu and uzu in the first puzzle, so I thought it could be a caesar shift where reu and uzu corresponded with and and did, which worked out if shifted by 17 originally. |
+| 6:50-7:10 | Aug 22 | Implement caesar shift for first puzzle.                                                                                                                                    |
 
 To add a new row to the table, click into a cell and then hit shift-enter.
+
--- src/Riddler.java
+++ src/Riddler.java
@@ -4,20 +4,43 @@
  * for Adventures in Algorithms
  * At Menlo School in Atherton, CA
  *
- * Completed by: YOUR NAME HERE
+ * Completed by: Zoe Sun
  */
 public class Riddler {
 
-    public String decryptOne(String encrypted) {
-        String decrypted = "";
+    public String decryptOne(String encrypted) { // TODO: test
+        String decrypted = ""; // Initialize empty string for decrypted string
+        char base; // Initialize base (depends on if upper/lowercase letter)
 
-        // TODO: Complete the decryptOne() function.
+        // Iterate over entire string to decrypt
+        for (int i = 0; i < encrypted.length(); i++) {
+            // Only need to decrypt letters
+            if (Character.isLetter(encrypted.charAt(i))) {
+                char letter = encrypted.charAt(i);
+                if (Character.isUpperCase(encrypted.charAt(i))) {
+                    base = 'A'; // base char is 'A' for uppercase letter
+                } else {
+                    base = 'a'; // base char is 'a' for lowercase letter
+                }
 
+                // Calculate decrypted ascii value of encrypted letters
+                int relativePosition =  (int) letter - (int) base; // Dist of encrypted letter from 'a' or 'A'
+                int shift = 9; // Shift by 9 to decrypt
+                int shiftedRelativePosition = (relativePosition + shift) % 26; // Distance of decrypted letter from 'a' or 'A' with wraparound
+                char shiftedChar = (char) (base + shiftedRelativePosition); // Convert ascii value back to char
+                decrypted = decrypted + shiftedChar;
+            } else { // Non-letter characters stay the same
+                decrypted = decrypted + encrypted.charAt(i);
+            }
+        }
         return decrypted;
     }
 
     public String decryptTwo(String encrypted) {
         String decrypted = "";
+        for (int i = 0; i < encrypted.length(); i++) {
+
+        }
 
         // TODO: Complete the decryptTwo() function.
 
@@ -40,3 +63,4 @@ public class Riddler {
         return decrypted;
     }
 }
+
