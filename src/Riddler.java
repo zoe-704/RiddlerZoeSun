f93f9282e0ecb7304763e13dc4e317f2131c2320
zoe-704 <sunzoey@gmail.com>
2025-08-27 18:03:14 -0700

    Update work log and iml file

--- src/Riddler.java
+++ src/Riddler.java
@@ -9,66 +9,78 @@
 public class Riddler {
 
     // Puzzle 1: Caesar Cipher
-    public String decryptOne(String encrypted) { // TODO: test
-        String decrypted = ""; // Initialize empty string for decrypted string
-        char base; // Initialize base (depends on if upper/lowercase letter)
-
+    public String decryptOne(String encrypted) {
+        String decrypted = "";
+        char base;
         // Iterate over entire string to decrypt
         for (int i = 0; i < encrypted.length(); i++) {
             // Only need to decrypt letters
             if (Character.isLetter(encrypted.charAt(i))) {
                 char letter = encrypted.charAt(i);
                 if (Character.isUpperCase(encrypted.charAt(i))) {
-                    base = 'A'; // base char is 'A' for uppercase letter
+                    // base char is 'A' for uppercase letter
+                    base = 'A';
                 } else {
-                    base = 'a'; // base char is 'a' for lowercase letter
+                    // base char is 'a' for lowercase letter
+                    base = 'a';
                 }
-
                 // Calculate decrypted ascii value of encrypted letters
-                int relativePosition =  (int) letter - (int) base; // Dist of encrypted letter from 'a' or 'A'
+                int relativePosition =  (int) letter - (int) base; // Dist of encrypted letter from base
                 int shift = 9; // Shift by 9 to decrypt
-                int shiftedRelativePosition = (relativePosition + shift) % 26; // Distance of decrypted letter from 'a' or 'A' with wraparound
+                int shiftedRelativePosition = (relativePosition + shift) % 26; // Dist including shift and wraparound
                 char shiftedChar = (char) (base + shiftedRelativePosition); // Convert ascii value back to char
-                decrypted = decrypted + shiftedChar;
-            } else { // Non-letter characters stay the same
-                decrypted = decrypted + encrypted.charAt(i);
+                decrypted += shiftedChar;
+            } else {
+                // Non-letter characters stay the same
+                decrypted += encrypted.charAt(i);
             }
         }
         return decrypted;
     }
+
     // Puzzle 2: Ascii values
-    public String decryptTwo(String encrypted) { // TODO: test
+    public String decryptTwo(String encrypted) {
         String decrypted = "";
         // Convert numbers from string into ints
         String[] splits = encrypted.split(" ");
         int[] nums = new int[splits.length];
+        // Parse integers from string
         for (int i = 0; i < splits.length; i++) {
             nums[i] = Integer.parseInt(splits[i]);
         }
+        // Convert integers from ascii value to char
         for (int i = 0; i < nums.length; i++) {
             char letter = (char) nums[i];
-            decrypted = decrypted + letter;
+            decrypted += letter;
         }
         return decrypted;
     }
 
-    public String decryptThree(String encrypted) { // TODO: test
+    // Puzzle 3: Binary 8-bit int
+    public String decryptThree(String encrypted) {
         String decrypted = "";
         // Iterate over each 8  bit binary sequence
         for (int i = 0; i < encrypted.length(); i+=8) {
             String bytes = encrypted.substring(i, i + 8);
-            // Convert from binary int to letter
+            // Convert from binary int to character
             char letter = (char) Integer.parseInt(bytes, 2);
-            decrypted = decrypted + letter;
+            decrypted += letter;
         }
         return decrypted;
     }
 
+    // Puzzle 4: Dingbat unicode
     public String decryptFour(String encrypted) {
         String decrypted = "";
-
-        // TODO: Complete the decryptFour() function.
-
+        for (int i = 0; i < decrypted.length(); i++) {
+            // Get the numerical value of the dingbat character
+            int dingbat = decrypted.charAt(i);
+            // Convert from unicode to ascii
+            int ascii = dingbat - 0x2700 + 45;
+            // Convert to character
+            char letter = (char) ascii;
+            decrypted += letter;
+        }
         return decrypted;
     }
 }
